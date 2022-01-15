25ea80f0d5ccd438a482edb929d96a85a0822485
kasumi teruya <all.splashing@gmail.com>
2022-01-15 13:56:47 +0900

    string

---
+++ ch02/string.ts
@@ -0,0 +1,9 @@
+const personName: string = 'Michael Jackson:'
+const blankName: string = ''
+
+const firstName: string = 'Michael'
+const lastName: string = 'Jackson'
+const fullName: string = `${firstName} ${lastName}`
+
+const newName: string = personName
+const nameAndCode: string = 'Michael Jackson' + 12345
