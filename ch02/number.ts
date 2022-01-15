0fe85eb68fe1dc59c0b309e510163a25bd99d25e
kasumi teruya <all.splashing@gmail.com>
2022-01-15 13:56:59 +0900

    number

---
+++ ch02/number.ts
@@ -0,0 +1,11 @@
+const age: number = 20
+const newAge: number = 0
+const weight: number = 60.5
+const latitude: number = -135
+
+const nan: number = 0 / 0
+const infinity: number = 1 / 0
+const minusInfinity = -1 / 0
+
+// const invalidBigIntNum: number = 9007199254740991n // エラー
+const validBigIntNum: bigint = 9007199254740991n // ES020
