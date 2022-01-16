272a5aa1ec688d7be84e15c82c4f6409424e875f
kasumi teruya <all.splashing@gmail.com>
2022-01-16 13:35:07 +0900

    boolean

---
+++ ch02/boolean.ts
@@ -0,0 +1,10 @@
+const isOpen: boolean = true
+const disablee: boolean = false
+
+// const isOpen: boolean = '' // エラー
+// const disabled: boolean = 0 // エラー
+// const hasId: boolean = undefined // エラー
+
+const id: string = ''
+const hasId1: boolean = Boolean(id)
+const hasId2: boolean = !!id
