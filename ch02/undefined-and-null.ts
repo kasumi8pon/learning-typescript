7af9b78e17916690f362021ed520b49d34077276
kasumi teruya <all.splashing@gmail.com>
2022-01-17 00:06:08 +0900

    null and undefined

---
+++ ch02/undefined-and-null.ts
@@ -0,0 +1,7 @@
+const noValueUndefined: undefined = undefined
+const noValueNull: null = null
+
+// let tanuki: string = 'Ponta'
+// strictNullChecks true
+// tanuki = undefined // エラー
+// tanuki = null // エラー
