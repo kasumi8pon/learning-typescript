f31d619a8fd097da5730fb0c091e987288e97d39
kasumi teruya <all.splashing@gmail.com>
2022-01-16 14:02:53 +0900

    array

---
+++ ch02/array.ts
@@ -0,0 +1,4 @@
+const list: number[] = [1, 2, 3]
+// list.push('Michael Jackson') // Argument of type 'string' is not assignable to parameter of type 'number'.
+
+const newList: Array<number> = [1, 2, 3]
