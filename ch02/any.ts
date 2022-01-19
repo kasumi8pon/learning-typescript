5142c6a4c7d2412740b0df4ba55a2518d2ae76d0
kasumi teruya <all.splashing@gmail.com>
2022-01-19 23:45:06 +0900

    any

---
+++ ch02/any.ts
@@ -0,0 +1,18 @@
+let anyValue: any = 'Michael Jackson'
+anyValue = 123
+anyValue = true
+anyValue = [1, 2, 3]
+anyValue = undefined
+anyValue = null
+anyValue = () => {
+  console.log('Hello!')
+}
+anyValue = {
+  name: 'Stevie Wonder',
+  age: 20
+}
+
+const string: any = 123
+string.toUpperCase()
+string.someUndefinedMethod()
+
