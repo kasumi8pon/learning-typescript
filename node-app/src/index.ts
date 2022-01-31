fdf3092dfba569a2371638dfdd9426fa9ba68409
kasumi teruya <all.splashing@gmail.com>
2022-01-31 23:24:35 +0900

    Add validation

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -27,6 +27,12 @@ class HitAndBlow {
   }
   async play() {
     const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+
+    if (!this.validate(inputArr)) {
+      printLine('無効な入力です')
+      await this.play()
+      return
+    }
     const result = this.check(inputArr)
 
     if (result.hit !== this.answer.length) {
@@ -59,6 +65,13 @@ class HitAndBlow {
       hit: hitCount,
       blow: blowCount
     }
+  }
+
+  private validate(inputArr: string[]) {
+    const isLengthValid = inputArr.length === this.answer.length
+    const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
+    const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) === i)
+    return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
   }
 }
 
