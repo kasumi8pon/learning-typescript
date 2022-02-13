7c361d16fb82a88a98a09fb420326c76a8220364
kasumi teruya <all.splashing@gmail.com>
2022-02-13 16:22:07 +0900

    Use abstract class

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -35,12 +35,12 @@ type NextAction = typeof nextActions[number]
 const gameTitles = ['hit and blow', 'janken'] as const
 type GameTitle = typeof gameTitles[number]
 type GameStore = {
-  [key in GameTitle]: HitAndBlow | Janken
+  [key in GameTitle]: Game
 }
 
 class GameProcedure {
   private currentGameTitle: GameTitle | '' = ''
-  private currentGame: HitAndBlow | Janken | null = null
+  private currentGame: Game | null = null
 
   constructor(private readonly gameStore: GameStore) {}
 
@@ -82,10 +82,16 @@ class GameProcedure {
   }
 }
 
+abstract class Game {
+  abstract setting(): Promise<void>
+  abstract play(): Promise<void>
+  abstract end(): void
+}
+
 const modes = ['normal', 'hard'] as const
 type Mode = typeof modes[number]
 
-class HitAndBlow {
+class HitAndBlow implements Game {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   private answer: string[] = []
   private tryCount = 0
@@ -173,7 +179,7 @@ class HitAndBlow {
 const jankenOptions = ['rock', 'paper', 'scissors'] as const
 type JankenOption = typeof jankenOptions[number]
 
-class Janken {
+class Janken implements Game {
   private rounds = 0
   private currentRound = 1
   private result = {
