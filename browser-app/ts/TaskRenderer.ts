5774b9fcd510644d3a2b84d4dd625e80ec9abb90
kasumi teruya <all.splashing@gmail.com>
2022-03-01 22:31:58 +0900

    Dragula

--- browser-app/ts/TaskRenderer.ts
+++ browser-app/ts/TaskRenderer.ts
@@ -1,7 +1,12 @@
+import dragula from 'dragula'
 import { Task } from './Task'
 
 export class TaskRenderer {
-  constructor(private readonly todoList: HTMLElement) { }
+  constructor(
+    private readonly todoList: HTMLElement,
+    private readonly doingList: HTMLElement,
+    private readonly doneList: HTMLElement
+  ) { }
 
   append(task: Task) {
     const { taskEl, deleteButtonEl } = this.render(task)
@@ -17,6 +22,15 @@ export class TaskRenderer {
     if (!taskEl) return
 
     this.todoList.removeChild(taskEl)
+  }
+
+  subscribeDragAndDrop() {
+    dragula([this.todoList, this.doingList, this.doneList]).on('drop', (el, target, source, sibling) => {
+      console.log(el)
+      console.log(target)
+      console.log(source)
+      console.log(sibling)
+    })
   }
 
   private render(task: Task) {
